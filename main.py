f166246d5f8f203889936749c17307dfd5b289a3
hendsuuu <sutrishendra07@gmail.com>
2023-06-26 13:39:07 +0700

    update

--- main.py
+++ main.py
@@ -16,9 +16,14 @@ st.sidebar.title("Navigasi")
 page = st.sidebar.radio("Pages",list(Tabs.keys()))
 
 #upload file dataset
-dataset = upload()
+dataset =  st.file_uploader("Choose your database", accept_multiple_files=False,label_visibility="hidden")
+if dataset is not None:
+    file_name = dataset
+else:
+    file_name = "iris.csv"
+
 #load dataset
-df,x,y = load_data(dataset)
+df,x,y = load_data(file_name)
 #kondisi call app function
 if page in ["Prediction","Visualisation"]:
     Tabs[page].app(df,x,y)
